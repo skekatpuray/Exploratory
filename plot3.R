f75e5eea705551814c06221bf0551d0add29e4df
skekatpuray <sachin_stock@yahoo.com>
2015-03-08 18:10:21 -0400

    checkin again for adding library code

--- plot3.R
+++ plot3.R
@@ -1,3 +1,6 @@
+library(sqldf)
+library(dplyr)
+
 df <- read.csv.sql("household_power_consumption.txt", sql = "select * from file where Date = '2/2/2007' or Date = '1/2/2007'",sep = ";", header=TRUE)
 df$DateTime <- strptime(paste(df$Date, df$Time), "%d/%m/%Y %H:%M:%S")
 
